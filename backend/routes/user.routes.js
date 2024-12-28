130daa8f050b3169018e8a3a50d4d1242078542c
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2024-12-28 23:29:16 +0530

    login route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,12 +6,19 @@ const userController = require('../controllers/user.controller')
 
 
 router.post('/register', [
-  body('fullName.firstName').isLength({ min: 3 }). withMessage('firstname must be of altleast 3 character'),
+  body('fullName.firstName').isLength({ min: 3 }).withMessage('firstname must be of altleast 3 character'),
   body('email').isEmail().withMessage('invalid email'),
   body('password').isLength({ min: 6 }).withMessage('password must be of altleast 6 character')
 ],
   userController.registerUser
-  
+
+);
+
+router.post('/login', [
+  body('email').isEmail().withMessage('invalid email'),
+  body('password').isLength({ min: 6 }).withMessage('password is invalid')
+],
+  userController.loginUser
 )
 
 
