071565bcd16a18fb1e3a0131bda54dc922d416cf
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-01-03 17:06:02 +0530

    captain register

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const connectToDb = require("./db/db")
 const userRoutes = require('./routes/user.routes')
 const cookies = require("cookie-parser")
+const captainRoutes = require("./routes/captainRoutes")
 
 connectToDb();
 
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.use('/user', userRoutes);
+app.use('/captain',captainRoutes)
 
 
 module.exports = app;
