72f319496d6daf99fc922fca769d13ba84b311dd
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-08-03 23:36:11 +0530

    removed the harcoded key for security purpose

--- backend/services/map.services.js
+++ backend/services/map.services.js
@@ -8,7 +8,7 @@ module.exports.getAddressCoordinate = async (address) => {
     try {
         const response = await axios.get(url);
         if (response.data.status === 'OK') {
-            const location = response.data.results[ 0 ].geometry.location;
+            const location = response.data.results[0].geometry.location;
             return {
                 ltd: location.lat,
                 lng: location.lng
@@ -37,11 +37,11 @@ module.exports.getDistanceTime = async (origin, destination) => {
         const response = await axios.get(url);
         if (response.data.status === 'OK') {
 
-            if (response.data.rows[ 0 ].elements[ 0 ].status === 'ZERO_RESULTS') {
+            if (response.data.rows[0].elements[0].status === 'ZERO_RESULTS') {
                 throw new Error('No routes found');
             }
 
-            return response.data.rows[ 0 ].elements[ 0 ];
+            return response.data.rows[0].elements[0];
         } else {
             throw new Error('Unable to fetch distance and time');
         }
@@ -57,18 +57,19 @@ module.exports.getAutoCompleteSuggestions = async (input) => {
         throw new Error('query is required');
     }
 
-    const apiKey = process.env.GOOGLE_MAPS_API||AIzaSyBykxsLjbu31hcpJBQSySBS_NvEWb0w_ao;
+    const apiKey = process.env.GOOGLE_MAPS_API;
     const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&key=${apiKey}`;
 
     try {
         const response = await axios.get(url);
+        console.error(response);
         if (response.data.status === 'OK') {
             return response.data.predictions.map(prediction => prediction.description).filter(value => value);
         } else {
             throw new Error('Unable to fetch suggestions');
         }
     } catch (err) {
-        console.error(err);
+
         throw err;
     }
 }
@@ -79,7 +80,7 @@ module.exports.getCaptainsInTheRadius = async (ltd, lng, radius) => {
     const captains = await captainModel.find({
         location: {
             $geoWithin: {
-                $centerSphere: [ [ ltd, lng ], radius / 6371 ]
+                $centerSphere: [[ltd, lng], radius / 6371]
             }
         }
     });
